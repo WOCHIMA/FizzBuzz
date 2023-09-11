c818fa4292ec7279c9fe876e467c947a6b7adb5b
WOCHIMA <matthewchen2021@gmail.com>
2023-09-11 17:14:08 -0400

    a

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,11 +1,14 @@
 public class Multiples {
     public static void main(String[] args) {
         int i=0;
+        int count=0;
         while(i<1000){
             if (i%3==0 || i%5==0){
                 System.out.println(i);
+                count++;
             }
             i++;
         }
+        System.out.println(count);
     }
 }
