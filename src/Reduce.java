3650b669f5448b3c6d47e288076fafad541529d8
WOCHIMA <matthewchen2021@gmail.com>
2023-09-11 14:30:36 -0400

    bhvjuhvuvvyh

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i=100;
+        int count=0;
+        while(i>1){
+            if(i%2==0){
+                i=i/2;
+            }
+            else{
+                i=i-1;
+            }
+            count++;
+        }
+        System.out.println(count);
+        }
+    }
+
